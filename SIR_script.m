c07c7353afe9385d5d333519c743115b3251b284
jp-mainente <75385965+jp-mainente@users.noreply.github.com>
2021-11-18 17:30:17 +0000

    Update SIR_script.m
    Updating

--- SIR_script.m
+++ SIR_script.m
@@ -22,44 +22,77 @@
 clear;close all;clc;
 % ----------------- Parameters -----------------
 sigma = 1/5.2; % Average incubation period is of 5.2 days for covid 19
-gamma = 1/18; % Average illness duration
-pop_size = 2e8;
-effect_rep_rate = 1.6;  %for now, it's fixed
-beta = gamma * effect_rep_rate;
 t_max = 700;
 time = 1:1:t_max;
+scenarios = 4;
+
+% Change here the Scenarios to be implemented
+effect_rep_rate = [1.6 2.16 3 4];                  % Effective Reproduction Rate
+gamma = 1/18;                           % Average illness duration
+
 
 % --------------- Simulation --------------
 % Lower case value define fraction of population in every stage
 
 % Initial values
+beta_vec = gamma * effect_rep_rate;
+
 i = randi([1 100],1)/10000;
 r = 0;
 e = i;   %random inition exposure
 s = 1-sum([i r e]);
 
-S_lt = zeros([1 t_max]);
-E_lt = zeros([1 t_max]);
-I_lt = zeros([1 t_max]);
-R_lt = zeros([1 t_max]);
-S_lt(1) = s * pop_size;
-E_lt(1) = e * pop_size;
-I_lt(1) = i * pop_size;
-R_lt(1) = r * pop_size;
+S_lt = zeros([t_max scenarios]);
+E_lt = zeros([t_max scenarios]);
+I_lt = zeros([t_max scenarios]);
+R_lt = zeros([t_max scenarios]);
 
 % Evolution
-for j =2:t_max
-    S_lt(j) = s * pop_size;
-    E_lt(j) = e * pop_size;
-    I_lt(j) = i * pop_size;
-    R_lt(j) = r * pop_size;
+for z = 1:scenarios
+    i = randi([1 100],1)/10000;              %random inition infection
+    r = 0;
+    e = i;  
+    s = 1-sum([i r e]);
+    S_lt(1,z) = s;
+    E_lt(1,z) = e;
+    I_lt(1,z) = i;
+    R_lt(1,z) = r;
 
-    dif_s = - beta * s * i;
-    dif_e = - dif_s - sigma * e;
-    dif_i = sigma * e - gamma * i;
+    beta = beta_vec(z);
 
-    s = s + dif_s;
-    e = e + dif_e;
-    i = i + dif_i;
-    r = 1 - sum([s e i]);
+    for j = 2:t_max
+        % calculate the time derivatives
+        dif_s = - beta * s * i;
+        dif_e = - dif_s - sigma * e;
+        dif_i = sigma * e - gamma * i;
+        
+        % update the states
+        s = s + dif_s;
+        e = e + dif_e;
+        i = i + dif_i;
+        r = 1 - sum([s e i]);
+        
+        %save at vector
+        S_lt(j,z) = s;
+        E_lt(j,z) = e;
+        I_lt(j,z) = i;
+        R_lt(j,z) = r;
+    end
 end
+
+figure("Name","Infection Comparison","Color","w")
+h1 = plot(I_lt(:,1));
+hold on
+h2 = plot(I_lt(:,2));
+hold on
+h3 = plot(I_lt(:,3));
+hold on
+h4 = plot(I_lt(:,4));
+hold off
+
+legend([h1 h2 h3 h4],["$\beta$ = " num2str(beta_vec(1))],...
+                      ["$\beta$ = " num2str(beta_vec(2))],...
+                      ["$\beta$ = " num2str(beta_vec(3))],...
+                      ["$\beta$ = " num2str(beta_vec(4))],"Interpreter","latex")
+
+
